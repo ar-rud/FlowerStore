432ba2c6162047da38a66edcac9b4f2808cc4b3a
ar-rud <ar194225@gmail.com>
2024-10-13 22:48:04 +0300

    implemented store class. added test for it

---
+++ src/test/java/flowers/FlowerStoreTest.java
@@ -0,0 +1,44 @@
+package flowers;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.List;
+import java.util.ArrayList;
+
+import org.example.flowers.Flower;
+import org.example.flowers.FlowerBucket;
+import org.example.flowers.FlowerColor;
+import org.example.flowers.FlowerPack;
+import org.example.flowers.FlowerType;
+import org.example.flowers.Store;
+
+public class FlowerStoreTest {
+    private Store flowerStore;
+    private final Flower flower = new Flower(80, FlowerColor.RED, 50, FlowerType.ROSE);
+    private final FlowerPack flowerPack = new FlowerPack(flower, 5);
+    private final Flower flower2 = new Flower(80, FlowerColor.YELLOW, 60, FlowerType.TULIP);
+    private final FlowerPack flowerPack2 = new FlowerPack(flower2, 5);
+    @BeforeEach
+    public void init(){
+        flowerStore = new Store();
+    }
+    @Test
+    public void testStore(){
+        FlowerBucket bucket = new FlowerBucket();
+        bucket.add(flowerPack);
+        bucket.add(flowerPack2);
+        FlowerBucket bucket2 = new FlowerBucket();
+        bucket2.add(flowerPack2);
+        FlowerPack flowerPack3 = new FlowerPack(flower2, 6);
+        FlowerBucket flowerBucket3 = new FlowerBucket();
+        flowerBucket3.add(flowerPack3);
+        flowerStore.add(bucket);
+        flowerStore.add(bucket2);
+
+        List<FlowerBucket> match = flowerStore.search(bucket2);
+        FlowerBucket flowerSearched = match.get(0);
+        assertEquals(bucket2, flowerSearched);
+        match = flowerStore.search(flowerBucket3);
+        assertEquals(match, new ArrayList<FlowerBucket>());
+    }
+}
